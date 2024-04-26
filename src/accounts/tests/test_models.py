ad48ea3504a02d60c89fa057c2a517c9ea926904
tyenn <tyennof@gmail.com>
2024-04-26 22:42:32 +0200

    Add Gitignore

--- src/accounts/tests/test_models.py
+++ src/accounts/tests/test_models.py
@@ -1,6 +1,7 @@
+from django.contrib.auth import get_user_model
 from django.test import TestCase
 
-from accounts.models import CustomUser
+from accounts.models import CustomUser, ShippingAddress
 from event_mgmt.models import Event
 
 
@@ -26,3 +27,66 @@ class UserTest(TestCase):
         self.user.add_to_cart(slug="event-test")
         self.assertEqual(self.user.cart.orders.count(), 1)
         self.assertEqual(self.user.cart.orders.first().quantity, 1)
+
+
+User = get_user_model()
+
+
+class CustomUserManagerTests(TestCase):
+    def test_create_user(self):
+        user = User.objects.create_user(email='test@example.com', password='foo')
+        self.assertEqual(user.email, 'test@example.com')
+        self.assertTrue(user.check_password('foo'))
+        self.assertFalse(user.is_superuser)
+        self.assertFalse(user.is_staff)
+
+    def test_create_user_no_email(self):
+        with self.assertRaises(ValueError):
+            User.objects.create_user(email=None, password='foo')
+
+    def test_create_superuser(self):
+        admin_user = User.objects.create_superuser(email='admin@example.com', password='foo')
+        self.assertTrue(admin_user.is_superuser)
+        self.assertTrue(admin_user.is_staff)
+        self.assertTrue(admin_user.is_active)
+
+
+class CustomUserTests(TestCase):
+    def setUp(self):
+        self.user = User.objects.create_user(email='user@example.com', password='foo')
+
+    def test_add_to_cart(self):
+        event = Event.objects.create(eventName='Concert', eventSlug='concert')
+        cart = self.user.add_to_cart(slug='concert')
+        self.assertEqual(cart.orders.count(), 1)
+
+
+class ShippingAddressTests(TestCase):
+    def setUp(self):
+        self.user = User.objects.create_user(email='user@example.com', password='foo')
+        self.user.stripe_id = "cus_PxYJCL0d0XrH3F"
+        self.address = ShippingAddress.objects.create(
+            user=self.user,
+            name="Domicile",
+            address_1="2 rue libre",
+            city="lyon",
+            zip_code="69000",
+            country='fr'
+        )
+
+    def test_address_str(self):
+        expected_address_str = "Domicile\n2 rue libre\n\nlyon, 69000\nFRANCE"
+        self.assertEqual(str(self.address), expected_address_str)
+
+    def test_set_default(self):
+        new_address = ShippingAddress.objects.create(
+            user=self.user,
+            name="Work",
+            address_1="123 Work St",
+            city="Worktown",
+            zip_code="12345",
+            country='us'
+        )
+        new_address.set_default()
+        self.assertTrue(new_address.default)
+        self.assertFalse(self.address.default)
