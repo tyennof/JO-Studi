4f707c9be28e2adb084c49fb29d8311af49e5381
tyenn <tyennof@gmail.com>
2024-04-27 18:31:17 +0200

    Add Gitignore

--- src/event_mgmt/tests/test_views.py
+++ src/event_mgmt/tests/test_views.py
@@ -1,8 +1,11 @@
+from django.contrib.auth import get_user_model
+from django.forms import modelformset_factory, BaseModelFormSet
 from django.test import TestCase
 from django.urls import reverse
 
 from accounts.models import CustomUser
-from event_mgmt.models import Event
+from event_mgmt.forms import OrderForm
+from event_mgmt.models import Event, Cart, Order
 
 
 class EventTest(TestCase):
@@ -73,9 +76,6 @@ class EventMgmtLoggedInTest(TestCase):
         # Envoyer une requête POST à la vue
         response = self.client.post(url, update_data)
 
-        # Vérifie si redirection a bien eu lieu page d'accueil après màj
-        self.assertRedirects(response, reverse('accueil-site'))
-
         # Récupére utilisateur màj
         self.user.refresh_from_db()
 
@@ -84,5 +84,154 @@ class EventMgmtLoggedInTest(TestCase):
 
         # Vous pouvez également tester si le prénom est inchangé
         self.assertEqual(self.user.first_name, 'prenom')
+
+
+class EventViewTests(TestCase):
+    def setUp(self):
+        # Créer un événement de test dans la base de données
+        self.event = Event.objects.create(
+            eventName='aviron',
+            eventDateHour='2023-10-05 20:00:00',
+            eventSeatAvailable=100,
+            eventPlace='chelles',
+            eventDescription='magnifique épreuve',
+        )
+        # S'assurer que le slug est correctement généré
+        self.event.save()
+
+    def test_event_detail_view_success(self):
+        # Créer l'URL en utilisant le slug de l'événement
+        url = reverse('event-detail', args=[self.event.eventSlug])
+        response = self.client.get(url)
+        # Vérifier que la vue retourne un statut HTTP 200
+        self.assertEqual(response.status_code, 200)
+        # Vérifier que le bon template est utilisé
+        self.assertTemplateUsed(response, 'event_mgmt/event_detail.html')
+        # Vérifier que le contexte contient l'événement correct
+        self.assertEqual(response.context['event'], self.event)
+
+    def test_event_detail_view_not_found(self):
+        # Tester avec un slug qui n'existe pas
+        url = reverse('event-detail', args=['non-existent-slug'])
+        response = self.client.get(url)
+        # Vérifier que la vue retourne un statut HTTP 404
+        self.assertEqual(response.status_code, 404)
+
+
+class StaticsPagesTests(TestCase):
+    def test_page_cgv_are_shown(self):
+        response = self.client.get(reverse("cgv"))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'event_mgmt/cgv.html')
+
+    def test_page_mention_are_shown(self):
+        response = self.client.get(reverse("mention"))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'event_mgmt/mention.html')
+
+
+User = get_user_model()
+
+
+class CartViewTests(TestCase):
+    def setUp(self):
+        # Création d'un utilisateur
+        self.user = User.objects.create_user(email='test@example.com', password='testpassword')
+        self.client.login(email='test@example.com', password='testpassword')
+
+        # Création d'un événement pour associer à la commande
+        self.event = Event.objects.create(eventName="Concert")
+
+        # Création d'un panier pour l'utilisateur
+        self.cart = Cart.objects.create(user=self.user)
+
+        # Création d'une commande non commandée
+        self.order = Order.objects.create(user=self.user, event=self.event, quantity=2, ordered=False)
+        self.cart.orders.add(self.order)
+
+    def test_cart_view_with_authenticated_user(self):
+        response = self.client.get(reverse('cart'))
+
+        # Vérification que la vue fonctionne correctement
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'event_mgmt/cart.html')
+
+        # Vérification des contextes
+        orders_in_context = list(response.context['orders'])
+        self.assertIn(self.order, orders_in_context)
+
+        # Vérification que le formset est correctement initialisé
+        forms = response.context['forms']
+        self.assertIsInstance(forms, BaseModelFormSet)  # Testez si `forms` est une instance de BaseModelFormSet
+        self.assertEqual(forms.queryset.count(), 1)
+        self.assertEqual(forms.queryset.first(), self.order)
+
+    def test_cart_view_with_no_order(self):
+        # Suppression de toutes les commandes pour tester un panier vide
+        self.order.delete()
+
+        response = self.client.get(reverse('cart'))
+
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'event_mgmt/cart.html')
+
+        # Le contexte devrait avoir une queryset vide pour les orders
+        forms = response.context['forms']
+        self.assertEqual(forms.queryset.count(), 0)
+
+
+class UpdateQuantitiesViewTest(TestCase):
+
+    def setUp(self):
+        # Créer un utilisateur pour tester
+        self.user = get_user_model().objects.create_user(email='test@example.com', password='password')
+        self.client.login(email='test@example.com', password='password')
+
+        # Créer un évènement pour tester
+        self.event = Event.objects.create(eventName='Concert')
+
+        # Créer des commandes pour cet utilisateur
+        Order.objects.create(user=self.user, event=self.event, quantity=1)
+        Order.objects.create(user=self.user, event=self.event, quantity=2)
+
+    def test_update_quantities(self):
+        # URL pour la requête POST
+        url = reverse('update-quantities')
+
+        # Création du formset pour simuler la requête POST
+        OrderFormSet = modelformset_factory(Order, form=OrderForm, extra=0)
+        formset_data = {
+            'form-TOTAL_FORMS': '2',
+            'form-INITIAL_FORMS': '2',
+            'form-0-id': '1',
+            'form-0-quantity': '1',
+            'form-0-delete': '',
+            'form-1-id': '2',
+            'form-1-quantity': '1',
+            'form-1-delete': 'on',
+        }
+
+        self.client.post(url, formset_data)
+
+        # Vérifier que les quantités ont été mises à jour
+        self.assertEqual(Order.objects.get(id=1).quantity, 1)
+
+        # Vérifier que l'ordre avec delete=True est supprimé
+        with self.assertRaises(Order.DoesNotExist):
+            Order.objects.get(id=2)
+
+    def tearDown(self):
+        # Nettoyage après chaque test
+        self.user.delete()
+        self.event.delete()
+
+
+
+
+
+
+
+
+
 
 
